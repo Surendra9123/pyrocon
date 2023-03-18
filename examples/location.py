2ce665244ce415ce8d44eec12133b46badf9d0c4
Surendra9123 <59458012+Surendra9123@users.noreply.github.com>
2023-03-18 12:31:24 +0530

    Update location.py

--- examples/location.py
+++ examples/location.py
@@ -1,9 +1,9 @@
 from pyrogram import filters,Client
 from pyrocon import patch
 
-bot = "6166806041:AAE_ZxX0VfcdkvcvTp4my-Vpjj_Xue8zYVo"
+bot = "61668fndmdcdkvcvTp4my-Vpjj_Xue8zYVo"
 
-app = Client("realblt",api_id = 1415407,api_hash = "118d68f87563b5c3b2b4ef7c4149f3d4",bot_token=bot)
+app = Client("realblt",api_id = 15407,api_hash = "563b5c3b2b4e4149f3d4",bot_token=bot)
 quiz = patch(app,clear=True)
 
 @app.on_message(filters.command("start"))  
