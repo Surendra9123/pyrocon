ee7d9048cf2a9b742cfab8acc51cf24f58167379
Surendra9123 <59458012+Surendra9123@users.noreply.github.com>
2023-03-18 12:44:25 +0530

    Create filters.py

---
+++ examples/filters.py
@@ -0,0 +1,14 @@
+from pyrogram import filters,Client
+from pyrocon import patch
+
+bot = "61668fndmdcdkvcvTp4my-Vpjj_Xue8zYVo"
+
+app = Client("realblt",api_id = 15407,api_hash = "563b5c3b2b4e4149f3d4",bot_token=bot)
+quiz = patch(app,clear=True)
+
+@app.on_message(filters.command("start"))  
+async def clear(client,message):
+    answer = await quiz.ask(message,"How are you bro?", filter=(filters.photo & filters.group))
+    await answer.reply(f"your answer is {ans.text}")
+
+app.run()
